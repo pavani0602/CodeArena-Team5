82f8f8428995f1714d99daa0676f7b9d9ce2b261
pavani0602 <pavanisajjana18@gmail.com>
2026-07-07 20:55:41 +0530

    Add Feature components to Home Page

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,9 +1,11 @@
 import Hero from "../../components/Hero/Hero";
+import Features from "../../components/Features/Features";
 
 function Home () {
     return (
         <main className="home-page">
       <Hero />
+      <Features />
     </main>
     );
 }
